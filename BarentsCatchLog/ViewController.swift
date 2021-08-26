734ff9c97e02f83e217b0765e82c175d9961557b
kneskoromny <kneskoromny@gmail.com>
2021-08-26 11:24:41 +0300

    Initial Commit

---
+++ BarentsCatchLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BarentsCatchLog
+//
+//  Created by Кирилл Нескоромный on 26.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
