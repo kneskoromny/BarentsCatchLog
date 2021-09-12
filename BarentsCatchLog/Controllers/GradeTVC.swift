b2cf673a317a75abd501f8f164b715af3f56e8b2
kneskoromny <kneskoromny@gmail.com>
2021-09-12 09:26:17 +0300

    код ревью DateVC, FishTVC, GradeTVC

--- BarentsCatchLog/Controllers/GradeTVC.swift
+++ BarentsCatchLog/Controllers/GradeTVC.swift
@@ -8,26 +8,26 @@
 import UIKit
 
 class GradeTVC: UITableViewController {
-    
-    let grades = ["0.5 -", "0.5 +", "0.5 - 1", "1 +", "1 -", "1 - 2", "2 - 3", "3 +", "5 +"]
-    
+    // MARK: - Public Properties
     var delegate: GradeTVCDelegate!
 
+    // MARK: - Private Properties
+    private let grades = ["без навески", "0.5 -", "0.5 +",
+                          "1 -", "0.5 - 1", "1 +",
+                          "1 - 2", "2 - 3", "3 +", "5 +"]
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    // MARK: - Table view data source
-
+    // MARK: - TableViewDataSource
     override func numberOfSections(in tableView: UITableView) -> Int {
         1
     }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         grades.count
     }
-
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "gradeCell", for: indexPath)
         let grade = grades[indexPath.row]
@@ -35,57 +35,10 @@ class GradeTVC: UITableViewController {
 
         return cell
     }
-    
+    // MARK: - TableViewDelegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let grade = grades[indexPath.row]
         delegate.valueDidChanged(to: grade)
         dismiss(animated: true)
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
