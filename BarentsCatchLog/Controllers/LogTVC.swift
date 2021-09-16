e660a676a81d691a01ca76fef72b2dab56c0021b
kneskoromny <kneskoromny@gmail.com>
2021-09-16 15:44:39 +0300

    добавил обновление журнала через refresh control

--- BarentsCatchLog/Controllers/LogTVC.swift
+++ BarentsCatchLog/Controllers/LogTVC.swift
@@ -9,6 +9,7 @@ import UIKit
 import CoreData
 
 class DailyCatch {
+    
     let date: String?
     let month: String?
     var fishes: [Fish]?
@@ -25,32 +26,43 @@ class LogTVC: UITableViewController {
     // MARK: - Public Properties
     lazy var coreDataStack = CoreDataStack(modelName: "BarentsCatchLog")
     var dailyCatch = [DailyCatch]()
+    var sortedDailyCatch = [DailyCatch]()
+    let tableRefreshControl: UIRefreshControl = {
+        let control = UIRefreshControl()
+        control.addTarget(self, action: #selector(update(sender:)), for: .valueChanged)
+        return control
+    }()
     
     // MARK: - Private Properties
     private var fishes: [Fish] = []
+    private var updatedFishes: [Fish] = []
     private var months = ["Янв", "Фев", "Мар", "Апр", "Май", "Июн",
                           "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"]
+    
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         let fetchRequest: NSFetchRequest<Fish> = Fish.fetchRequest()
         do {
             fishes = try coreDataStack.managedContext.fetch(fetchRequest)
-            print("fishes in LOG \(fishes.count)")
+            //print("fishes in LOG \(fishes.count)")
         } catch let error as NSError {
             print("Fetch error: \(error) description: \(error.userInfo)")
         }
-        print("Its a fishes count - \(fishes.count)")
+        //print("Its a fishes count - \(fishes.count)")
         let convertedFishes = fishes.sorted(by: { ($0.date)?.compare($1.date!) == .orderedDescending})
-
+        
         divideByDate(from: convertedFishes)
         print("Its a dailyCatch count - \(dailyCatch.count)")
-        tableView.reloadData()
+        //tableView.reloadData()
         dailyCatch.forEach { dailyCatch in
-            print("Its a dailyCatch  - \(dailyCatch.fishes?.first)")
-
+            //print("Its a dailyCatch  - \(dailyCatch.fishes?.first)")
         }
+        tableView.refreshControl = tableRefreshControl
+        
     }
+    // MARK: - IB Actions
+    
     
     // MARK: - Private Methods
     private func divideByDate(from fishes: [Fish]) {
@@ -62,55 +74,79 @@ class LogTVC: UITableViewController {
             fishDate = String(describing: components.day!)
             fishMonth = String(describing: components.month!)
         }
-        // не работает если вносишь один тип рыбы
-            for fish in fishes {
-                var currentFishDate: String?, currentFishMonth: String?
-                if let date = fish.date {
-                    let components = Calendar.current.dateComponents([.day, .month], from: date)
-                    currentFishDate = String(describing: components.day!)
-                    currentFishMonth = String(describing: components.month!)
-                }
-                print("Inside forIn - fishDate \(String(describing: fishDate))")
-                print("Inside forIn - fish.date \(String(describing: currentFishDate))")
-                if currentFishDate == fishDate {
-                    dividedFishes.append(fish)
-                } else {
-                    let catchDaily = DailyCatch(date: fishDate!, month: fishMonth!, fishes: dividedFishes)
-                    dailyCatch.append(catchDaily)
-                    isFishDidAdded.toggle()
-                    dividedFishes.removeAll()
-                    dividedFishes.append(fish)
-                    isFishDidAdded.toggle()
-                    fishDate = currentFishDate
-                    fishMonth = currentFishMonth
-                }
+        // не работает если вносишь один тип рыбы - РЕШЕНО ЧЕРЕЗ REFRESH CONTROL
+        for fish in fishes {
+            var currentFishDate: String?, currentFishMonth: String?
+            if let date = fish.date {
+                let components = Calendar.current.dateComponents([.day, .month], from: date)
+                currentFishDate = String(describing: components.day!)
+                currentFishMonth = String(describing: components.month!)
             }
+            print("Inside forIn - fishDate \(String(describing: fishDate))")
+            print("Inside forIn - fish.date \(String(describing: currentFishDate))")
+            if currentFishDate == fishDate {
+                dividedFishes.append(fish)
+            } else {
+                let catchDaily = DailyCatch(date: fishDate!, month: fishMonth!, fishes: dividedFishes)
+                dailyCatch.append(catchDaily)
+                isFishDidAdded.toggle()
+                dividedFishes.removeAll()
+                dividedFishes.append(fish)
+                isFishDidAdded.toggle()
+                fishDate = currentFishDate
+                fishMonth = currentFishMonth
+            }
+        }
+        print("Divided fishes count - \(dividedFishes.count)")
         if !dividedFishes.isEmpty && !isFishDidAdded {
+            // добавить проверку на nil
             let catchDaily = DailyCatch(date: fishDate!, month: fishMonth!, fishes: dividedFishes)
             dailyCatch.append(catchDaily)
         }
+        sortedDailyCatch = dailyCatch.sorted { catch1, catch2 in
+            let date1 = Int(catch1.date!), date2 = Int(catch2.date!)
+            return date1! > date2!
         }
     }
-
-
-    // MARK: - TableViewDataSource
+    // обновляет table view при свайпе вниз
+    @objc private func update(sender: UIRefreshControl) {
+        let fetchRequestForUpdate: NSFetchRequest<Fish> = Fish.fetchRequest()
+        do {
+            updatedFishes = try coreDataStack.managedContext.fetch(fetchRequestForUpdate)
+            //print("UPDATED FISHES COUNT \(updatedFishes.count)")
+        } catch let error as NSError {
+            print("Fetch error: \(error) description: \(error.userInfo)")
+        }
+        //print("FISHES COUNT - \(fishes.count)")
+        
+        let convFishes = Set(fishes.sorted(by: { ($0.date)?.compare($1.date!) == .orderedDescending}))
+        let convUpdFishes = Set(updatedFishes.sorted(by: { ($0.date)?.compare($1.date!) == .orderedDescending}))
+        let update = Array(convFishes.symmetricDifference(convUpdFishes))
+        //print("UPDATE COUNT - \(update.count)")
+        divideByDate(from: update)
+        fishes = updatedFishes
+        tableView.reloadData()
+        sender.endRefreshing()
+    }
+}
+// MARK: - TableViewDataSource
 extension LogTVC {
     override func numberOfSections(in tableView: UITableView) -> Int {
-        dailyCatch.count
+        sortedDailyCatch.count
     }
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let headerView = UIView.init(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 40))
-                
+        
         let figureLabel = UILabel()
         figureLabel.frame = CGRect(x: 5, y: 0, width: 30, height: 20)
-        figureLabel.text = dailyCatch[section].date!
+        figureLabel.text = sortedDailyCatch[section].date!
         figureLabel.textAlignment = .center
         figureLabel.font = .systemFont(ofSize: 16)
         figureLabel.textColor = UIColor(red: 72/255, green: 159/255, blue: 248/255, alpha: 1)
-    
+        
         let textLabel = UILabel()
         textLabel.frame = CGRect(x: 5, y: 20, width: 30, height: 20)
-        if let monthFigure = Int(dailyCatch[section].month!) {
+        if let monthFigure = Int(sortedDailyCatch[section].month!) {
             textLabel.text = months[monthFigure - 1]
         }
         textLabel.textAlignment = .center
@@ -126,14 +162,14 @@ extension LogTVC {
         40
     }
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        dailyCatch[section].date!
+        sortedDailyCatch[section].date!
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        dailyCatch[section].fishes?.count ?? 0
+        sortedDailyCatch[section].fishes?.count ?? 0
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "logCell", for: indexPath) as! LogCell
-        let fish = dailyCatch[indexPath.section].fishes?[indexPath.row]
+        let fish = sortedDailyCatch[indexPath.section].fishes?[indexPath.row]
         cell.fishlabel?.text = fish?.name
         if let perDay = fish?.perDay {
             cell.frzBoardLabel.text = String(format: "%.0f", perDay) + " кг"
@@ -147,26 +183,26 @@ extension LogTVC {
 extension LogTVC {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            guard let fish = dailyCatch[indexPath.section].fishes?[indexPath.row] else { return }
-            let daily = dailyCatch[indexPath.section]
+            guard let fish = sortedDailyCatch[indexPath.section].fishes?[indexPath.row] else { return }
+            let daily = sortedDailyCatch[indexPath.section]
             let indexSet = IndexSet(arrayLiteral: indexPath.section)
             
-            if let index = dailyCatch[indexPath.section].fishes?.firstIndex(of: fish) {
-                if dailyCatch[indexPath.section].fishes!.count > 1 {
-                    dailyCatch[indexPath.section].fishes?.remove(at: index)
+            if let index = sortedDailyCatch[indexPath.section].fishes?.firstIndex(of: fish) {
+                if sortedDailyCatch[indexPath.section].fishes!.count > 1 {
+                    sortedDailyCatch[indexPath.section].fishes?.remove(at: index)
                     tableView.deleteRows(at: [indexPath], with: .left)
                 } else {
-                    if let index2 = dailyCatch.firstIndex(where: { dailyCatch in
+                    if let index2 = sortedDailyCatch.firstIndex(where: { dailyCatch in
                         dailyCatch === daily
                     }) {
-                        dailyCatch[indexPath.section].fishes?.remove(at: index)
-                        print("1")
+                        sortedDailyCatch[indexPath.section].fishes?.remove(at: index)
+                        //print("1")
                         tableView.deleteRows(at: [indexPath], with: .left)
-                        print("3")
-                        dailyCatch.remove(at: index2)
-                        print("2")
+                        //print("3")
+                        sortedDailyCatch.remove(at: index2)
+                        //print("2")
                         tableView.deleteSections(indexSet, with: .left)
-                        print("4")
+                        //print("4")
                     }
                 }
                 coreDataStack.managedContext.delete(fish)
@@ -176,3 +212,4 @@ extension LogTVC {
         }
     }
 }
+
