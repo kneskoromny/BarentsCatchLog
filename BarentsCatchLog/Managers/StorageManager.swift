eb11ab5596df1f4abb91ecc2a56c935f6795fd03
kneskoromny <kneskoromny@gmail.com>
2021-09-22 20:49:55 +0300

    методы в StorageManager

--- BarentsCatchLog/Managers/StorageManager.swift
+++ BarentsCatchLog/Managers/StorageManager.swift
@@ -12,7 +12,24 @@ class StorageManager {
     static let shared = StorageManager()
     
     private let userDefaults = UserDefaults.standard
-    private let key = "grade"
+    private let gradeKey = "grade"
     
     private init() {}
+    
+    func fetchGrades() -> [String] {
+            if let grades = userDefaults.value(forKey: gradeKey) as? [String] {
+                return grades
+            }
+            return []
+        }
+    func save(grade: String) {
+            var grades = fetchGrades()
+            grades.append(grade)
+            userDefaults.set(grades, forKey: gradeKey)
+        }
+    func deleteGrade(at index: Int) {
+            var grades = fetchGrades()
+            grades.remove(at: index)
+            userDefaults.set(grades, forKey: gradeKey)
+        }
 }
