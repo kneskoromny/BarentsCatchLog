9cee755c4afe09149e4755aea2abcee944ce6972
kneskoromny <kneskoromny@gmail.com>
2021-09-22 19:27:10 +0300

    добавил переход на контроллеры польз настроек

---
+++ BarentsCatchLog/Controllers/NavControllers/SettingsListTVCNC.swift
@@ -0,0 +1,11 @@
+//
+//  SettingsListTVCNC.swift
+//  BarentsCatchLog
+//
+//  Created by Кирилл Нескоромный on 22.09.2021.
+//
+
+import UIKit
+
+class SettingsListTVCNC: UINavigationController {
+}
---
+++ BarentsCatchLog/Controllers/SettingsVC.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsVC.swift
+//  BarentsCatchLog
+//
+//  Created by Кирилл Нескоромный on 22.09.2021.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
