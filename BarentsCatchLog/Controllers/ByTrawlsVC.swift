06561ccde4302b6058c02fb338c6353cc69b3000
kneskoromny <kneskoromny@gmail.com>
2021-09-26 14:44:38 +0300

    добавил ByTrawlsVC и файл

---
+++ BarentsCatchLog/Controllers/ByTrawlsVC.swift
@@ -0,0 +1,17 @@
+//
+//  ByTrawlsVC.swift
+//  BarentsCatchLog
+//
+//  Created by Кирилл Нескоромный on 26.09.2021.
+//
+
+import UIKit
+
+class ByTrawlsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+ 
+
+}
